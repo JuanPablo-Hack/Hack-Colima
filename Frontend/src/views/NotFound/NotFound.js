271e1910e02703b213455c42a592690ca2eeb687
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2020-04-09 15:04:29 -0500

    Subir pagina de Error 404

--- Frontend/src/views/NotFound/NotFound.js
+++ Frontend/src/views/NotFound/NotFound.js
@@ -35,11 +35,11 @@ const NotFound = () => {
         >
           <div className={classes.content}>
             <Typography variant="h1">
-              404: The page you are looking for isn’t here
+              404: La página que estás buscando no está aquí.
             </Typography>
             <Typography variant="subtitle2">
-              You either tried some shady route or you came here by mistake.
-              Whichever it is, try using the navigation
+            Intentaste alguna ruta turbia o viniste aquí por error.
+            Cualquiera que sea, intente usar la navegación
             </Typography>
             <img
               alt="Under development"
